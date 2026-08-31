b15a92d60e57ef1b6b24092088b33712fde742fe
Supreme Lord <261223650+kenkenny381-commits@users.noreply.github.com>
2026-08-31 17:09:54 +0300

    Update bot settings and version information

--- settings.js
+++ settings.js
@@ -5,17 +5,17 @@
 */
 
 const settings = {
-  packname: '𝐒𝐏𝐎𝐈𝐋𝐄𝐑-𝐓𝐄𝐂𝐇 ',
-  author: '𝐒𝐏𝐎𝐈𝐋𝐄𝐑-𝐓𝐄𝐂𝐇 ',
-  botName: "𝐒𝐏𝐎𝐈𝐋𝐄𝐑-𝐓𝐄𝐂𝐇 ",
-  botOwner: '𝐒𝐏𝐎𝐈𝐋𝐄𝐑-𝐓𝐄𝐂𝐇 ', // Your name
-  ownerNumber: process.env.OWNER_NUMBER || '255675421210', //Set your number here without + symbol, just add country code & number without any space
+  packname: '𝐒𝐔𝐏𝐑𝐄𝐌𝐄-𝐓𝐄𝐂𝐇 ',
+  author: '𝐒𝐔𝐏𝐑𝐄𝐌𝐄 𝐋𝐎𝐑𝐃𝐓𝐄𝐂𝐇 ',
+  botName: "𝐒𝐔𝐏𝐑𝐄𝐌𝐄-𝐋𝐎𝐑𝐃𝐓𝐄𝐂𝐇 ",
+  botOwner: '𝐒ᵘᵖʳᵉᵐᵉ ᴸᴼᴿᴰ𝐓𝐄𝐂𝐇 ', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '254143914610'''''''''''ur number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.0",
+  version: "2.3.4",
   updateBranch: 'main',
   updateZipToken: '',
   updateZipUrl: "https://github.com/adevos-x-tech/adevosxbot/archive/refs/heads/main.zip",
